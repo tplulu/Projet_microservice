446f160547018390e341f8b5b0a7c5664feed829
TPLulu <thibault.piotrowski@gmail.com>
2023-10-19 16:44:48 +0200

    ajout test et promethus

---
+++ app_test/tests/test_app.py
@@ -0,0 +1,21 @@
+import pytest
+# from app_read_product.app_read_product.app import app
+
+# # Test de la route /read_product
+# def test_read_product():
+#     client = app.test_client()
+#     response = client.get('/read_product')
+
+#     # Vérifiez que la réponse a un code de statut HTTP 200 (OK)
+#     assert response.status_code == 200
+
+#     # Vérifiez que la réponse contient les données attendues
+#     expected_data = [
+#         {},
+#         # Ajoutez d'autres données de produits attendues si nécessaire
+#     ]
+
+#     response_data = response.get_json()  # Récupérez les données JSON de la réponse
+#     assert response_data['products'] == expected_data
+
+# Vous pouvez ajouter d'autres tests ici si nécessaire
